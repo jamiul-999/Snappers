9dac1bf7e520ca8124338903165ae8bfd085e3b5
charlie <jhdhj999@gmail.com>
2025-05-23 20:50:34 +0600

    Edit option added in views and templates

--- snappers/account/views.py
+++ snappers/account/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+#from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import LoginForm, UserRegistrationForm
+from django.shortcuts import render, redirect
+from .forms import (
+    LoginForm, 
+    UserRegistrationForm,
+    UserEditForm,
+    ProfileEditForm
+)
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -45,6 +52,8 @@ def register(request):
             )
             # Save the User object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 "account/register_done.html",
@@ -56,4 +65,34 @@ def register(request):
         request,
         "account/register.html",
         {"user_form": user_form}
+    )
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            #messages.success(request, "Profile updated successfully!")
+            return redirect("dashboard")
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        
+    return render(
+        request,
+        "account/edit.html",
+        {
+            "user_form": user_form,
+            "profile_form": profile_form
+        }
     )
