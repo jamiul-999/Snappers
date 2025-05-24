36effcaf8886c08868f73985c618cea87b3275bf
charlie <jhdhj999@gmail.com>
2025-05-24 22:29:33 +0600

    Message added for updated profile

--- snappers/account/views.py
+++ snappers/account/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-#from django.contrib import messages
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import (
@@ -82,8 +82,13 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            #messages.success(request, "Profile updated successfully!")
-            return redirect("dashboard")
+            messages.success(
+                request, 
+                "Profile updated successfully!"
+            )
+        else:
+            messages.error(request, "Error updating your profile")
+            
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
